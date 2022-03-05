fb0244b40341a75ace11c3d6997ac6ef51c063e1
tzcawa <66556786+tzcawa@users.noreply.github.com>
2022-03-05 19:01:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 {
-	let uid=window._feInjection.currentUser.uid;
-	let cur=0,sum=-1,v=$("<span style='-webkit-touch-callout:none;-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none;-khtml-user-select:none;user-select:none;position:fixed;top:0;left:0;backdrop-filter:blur(5px);color:#000;display:block;text-align:center;z-index:114514'>正在准备，请稍候</span>");
-	v.css({"width":window.innerWidth+"px","height":window.innerHeight+"px","line-height":window.innerHeight+"px"});
-	$(document.body).append(v);
-	window.addEventListener("resize",function(){
-		v.css({"width":window.innerWidth+"px","height":window.innerHeight+"px","line-height":window.innerHeight+"px"});
-	})
+	let uid=_feInjection.currentUser.uid,cur,sum,v;
+	function wid(w=0){
+		return w?innerWidth:innerHeight;
+	}
+	function work(){
+		cur=0;sum=-1;v=$("<span style='position:fixed;top:0;left:0;backdrop-filter:blur(5px);color:#000;display:block;text-align:center;z-index:114514'>正在准备，请稍候</span>");
+		v.css({"width":wid(1)+"px","height":wid()+"px",lineHeight:wid()+"px"});
+		$(document.body).append(v);
+		addEventListener("resize",function(){
+			v.css({"width":wid(1)+"px","height":wid()+"px",lineHeight:wid()+"px"});
+		});
+		exec();
+	}
 	function exec(){
 		$.get("/api/feed/list?user="+uid).success(function(d){
 			if(sum==-1){
 				sum=d.feeds.count;
-				v.text("共 "+sum+" 条犇犇，已删除 "+cur+" 条");
+				v.text("共"+sum+"条犇犇，已删除"+cur+"条");
 			}
 			if(d.feeds.result.length==0){
 				v.text("已经删除完毕qwq");
@@ -19,7 +25,7 @@
 			}
 			let i=0,int=setInterval(function(){
 				$.post("/api/feed/delete/"+d.feeds.result[i].id).success(function(){
-					v.text("共 "+sum+" 条犇犇，已删除 "+(++cur)+" 条");
+					v.text("共"+sum+"条犇犇，已删除"+(++cur)+"条");
 					if(++i>=d.feeds.result.length){
 						if(d.feeds.count>20){
 							setTimeout(function(){exec();},500);
@@ -33,5 +39,9 @@
 			},500);
 		});
 	}
-	exec();
+	if(!$.fn){
+		v=document.createElement("script");v.src="//cdn.luogu.com.cn/js/jquery-2.1.1.min.js";
+		v.onload=work;
+		document.body.append(v);
+	}else work();
 }
