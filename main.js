1643883cd1bbc74629a13779bd05cb43a12c3fa9
tzcawa <66556786+tzcawa@users.noreply.github.com>
2022-03-05 09:42:54 +0800

    Create main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 {
 	let uid=$("span.wrapper.hover>span>a").attr("href").match("/user/([0-9]+)")[1];
-	let cur=0,sum=-1,v=$("<span style='-webkit-touch-callout:none;-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none;-khtml-user-select:none;user-select:none;position:fixed;top:0;left:0;backdrop-filter:blur(5px);color:#000;display:block;text-align:center;z-index:114514'>\u6b63\u5728\u51c6\u5907\uff0c\u8bf7\u7a0d\u5019</span>");
+	let cur=0,sum=-1,v=$("<span style='-webkit-touch-callout:none;-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none;-khtml-user-select:none;user-select:none;position:fixed;top:0;left:0;backdrop-filter:blur(5px);color:#000;display:block;text-align:center;z-index:114514'>正在准备，请稍候</span>");
 	v.css({"width":window.innerWidth+"px","height":window.innerHeight+"px","line-height":window.innerHeight+"px"});
 	$(document.body).append(v);
 	window.addEventListener("resize",function(){
@@ -11,29 +11,32 @@
 		$.get("/api/feed/list?user="+uid).success(function(d){
 			if(sum==-1){
 				sum=d.feeds.count;
-				v.text("\u5171 "+sum+" \u6761\u7287\u7287\uff0c\u5df2\u5220\u9664 "+cur+" \u6761");
+				v.text("共 "+sum+" 条犇犇，已删除 "+cur+" 条");
 			}
 			if(d.feeds.result.length==0){
-				v.text("\u5df2\u7ecf\u5220\u9664\u5b8c\u6bd5qwq");
+				v.text("已经删除完毕qwq");
 				v.animate({"opacity":"0"},3000,function(){
 					v.remove();
+					location.reload();
 				})
 				return;
 			}
 			let i=0,int=setInterval(function(){
-				$.post("/api/feed/delete/"+d.feeds.result[i].id);
-				v.text("\u5171 "+sum+" \u6761\u7287\u7287\uff0c\u5df2\u5220\u9664 "+(++cur)+" \u6761");
-				if(++i>=d.feeds.result.length){
-					if(d.feeds.count>20){
-						exec();
-					}else{
-						v.text("\u5df2\u7ecf\u5220\u9664\u5b8c\u6bd5qwq");
-						v.animate({"opacity":"0"},3000,function(){
-							v.remove();
-						});
+				$.post("/api/feed/delete/"+d.feeds.result[i].id).success(function(){
+					v.text("共 "+sum+" 条犇犇，已删除 "+(++cur)+" 条");
+					if(++i>=d.feeds.result.length){
+						if(d.feeds.count>20){
+							exec();
+						}else{
+							v.text("已经删除完毕qwq");
+							v.animate({"opacity":"0"},3000,function(){
+								v.remove();
+								location.reload();
+							});
+						}
+						clearInterval(int);
 					}
-					clearInterval(int);
-				}
+				});
 			},200);
 		});
 	}
