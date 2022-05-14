578efbc162c6db4fc4016cb99b514868a932b21c
tzcawa <66556786+tzcawa@users.noreply.github.com>
2022-05-14 12:20:31 +0800

    Add error exception; decrease size

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 {
+  const p=400;
 	let uid=_feInjection.currentUser.uid,cur,sum,v;
-	function wid(w=0){
-		return w?innerWidth:innerHeight;
-	}
-	cur=0;sum=-1;v=$("<span style='position:fixed;top:0;left:0;backdrop-filter:blur(5px);color:#000;display:block;text-align:center;z-index:114514'>正在准备，请稍候</span>");
-	v.css({"width":wid(1)+"px","height":wid()+"px",lineHeight:wid()+"px"});
-	$(document.body).append(v);
-	addEventListener("resize",function(){
-		v.css({"width":wid(1)+"px","height":wid()+"px",lineHeight:wid()+"px"});
-	});
-        exec();
+	cur=0;sum=-1;v=$("<div style='position:fixed;width:100%;height:100%;padding-top:5em;font-size:3em;top:0;left:0;backdrop-filter:blur(5px);color:#000;display:block;text-align:center;z-index:114514'>正在准备，请稍候</div>");
+	$("body").append(v);
+  function sto(f,t){setTimeout(f,t);}
 	function exec(){
 		$.get("/api/feed/list?user="+uid,function(d){
 			if(sum==-1){
@@ -18,23 +12,30 @@
 			}
 			if(d.feeds.result.length==0){
 				v.text("已经删除完毕qwq");
-				setTimeout(function(){location.reload();},1000);
+				sto(function(){location.reload();},1000);
 				return;
 			}
-			let i=0,int=setInterval(function(){
-				$.post("/api/feed/delete/"+d.feeds.result[i].id,function(){
-					v.text("共"+sum+"条犇犇，已删除"+(++cur)+"条");
-					if(++i>=d.feeds.result.length){
-						if(d.feeds.count>20){
-							setTimeout(function(){exec();},500);
-						}else{
-							v.text("已经删除完毕qwq");
-							setTimeout(function(){location.reload();},1000);
-						}
-						clearInterval(int);
-					}
-				});
-			},500);
-		});
+      function delb(i){
+        if(i>=d.feeds.result.length){
+          if(d.feeds.count>20) sto(exec,p);
+          else{
+            v.text("已经删除完毕qwq");
+            sto(function(){location.reload();},1000);
+          }
+          return;
+        }
+        $.post("/api/feed/delete/"+d.feeds.result[i].id,function(d){
+          if(d.status!=200) sto(function(){delb(i);},p);
+          else{
+            v.text("共"+sum+"条犇犇，已删除"+(++cur)+"条");
+            sto(function(){delb(i+1)},p);
+          }
+        }).error(function(){
+          sto(function(){delb(i);},p);
+        });
+      }
+      delb(0);
+		}).error(function(){sto(exec,500)});
 	}
+  exec();
 }
